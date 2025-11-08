ea66f2487d3a21bfbe6aa6bb3ea948909c94569c
miyuko <miyuko@calayucu.com>
2025-11-08 21:51:23 +0000

    Use a terminal theme with lighter colors against a black background.

--- src/terminal.ts
+++ src/terminal.ts
@@ -29,6 +29,35 @@ export class Terminal {
             // like line-height.
             fontFamily: parentContainerStyles.fontFamily,
             fontSize: Number(parentContainerStyles.fontSize.replace(/px$/, '')),
+
+            theme: {
+                black:   '#7a828e',
+                red:     '#ff9492',
+                green:   '#26cd4d',
+                yellow:  '#f0b72f',
+                blue:    '#71b7ff',
+                magenta: '#cb9eff',
+                cyan:    '#39c5cf',
+                white:   '#d9dee3',
+
+                brightBlack:   '#9ea7b3',
+                brightRed:     '#ffb1af',
+                brightGreen:   '#4ae168',
+                brightYellow:  '#f7c843',
+                brightBlue:    '#91cbff',
+                brightMagenta: '#dbb7ff',
+                brightCyan:    '#56d4dd',
+                brightWhite:   '#ffffff',
+
+                background: '#0a0c10',
+                foreground: '#f0f3f6',
+
+                cursor:       '#71b7ff',
+                cursorAccent: '#71b7ff',
+
+                selectionBackground: '#f0f3f6',
+                selectionForeground: '#0a0c10',
+            },
         });
         xterm.open(element);
         this.#xterm = xterm;
